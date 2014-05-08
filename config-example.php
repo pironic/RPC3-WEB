b0639c351b1e433f409832f68f0f6197a9c8405f
Michael Writhe <michael@writhem.com>
2014-05-08 16:46:21 -0600

    inital commit, nope. no documentation here.

---
+++ config-example.php
@@ -0,0 +1,14 @@
+<?
+// these will be used if you're not using a Crowd Auth server or there is a problem with it.
+$failover_username = "root";
+$failover_password = "password";
+
+// Enter the Crowd Auth server details here, or leave it if you dont have one. It'll error out and failover to the user/pass above.
+$crowd_app_name = 'rpc-manager';
+$crowd_app_password = '';
+$crowd_url = 'http://crowd.YOUR.DOMAIN/crowd/services/SecurityServer?wsdl';
+
+// The details of how the script can access the RPC.
+$rpc_ip = '192.168.1.2';
+$rpc_port = '23';
+
